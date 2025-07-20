2e9d4c0e4e54f28a134b7b0e910eef5d77cb898e
TongTong <tongtong950313@163.com>
2025-07-21 00:36:15 +0800

    调通A2A代码

--- a2a/test/main.py
+++ a2a/test/main.py
@@ -1,4 +1,10 @@
 from a2a.types import AgentCard, AgentSkill, AgentCapabilities
+from server import CodingAgentExecutor, CodingAgent
+from a2a.server.apps import A2AStarletteApplication
+from a2a.server.request_handlers import DefaultRequestHandler
+import os
+from a2a.server.tasks import InMemoryTaskStore
+import uvicorn
 
 if __name__ == "__main__":
     skill = AgentSkill(
@@ -12,7 +18,7 @@ if __name__ == "__main__":
     coding_agent_card = AgentCard(
         name="代码智能体",
         description="编写代码的智能体",
-        url="http://localhost:8765/",
+        url="http://localhost:9999/",
         capabilities=AgentCapabilities(streaming=True),
         skills=[skill],
         defaultInputModes=["text"],
@@ -20,5 +26,19 @@ if __name__ == "__main__":
         version="1.0.0",
     )
 
-    print(type(skill))
-    print(type(coding_agent_card))
+    request_handler = DefaultRequestHandler(
+        agent_executor=CodingAgentExecutor(
+            agent=CodingAgent(
+                api_key=os.getenv("DASHSCOPE_API_KEY"),
+                base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+            )
+        ),
+        task_store=InMemoryTaskStore(),
+    )
+
+    server = A2AStarletteApplication(
+        agent_card=coding_agent_card,
+        http_handler=request_handler,
+    )
+
+    uvicorn.run(server.build(), host="0.0.0.0", port=9999)
--- a2a/test/server.py
+++ a2a/test/server.py
@@ -1,3 +1,4 @@
+from email import message
 from a2a.server.agent_execution import AgentExecutor
 from openai import AsyncOpenAI
 from typing import Literal, Optional, List
@@ -55,12 +56,23 @@ class CodingAgent:
 
 
 class CodingAgentExecutor(AgentExecutor):
+    def __init__(self, agent: CodingAgent) -> None:
+        self.agent = agent
+
     # 必须实现execute和cancel方法
     async def execute(self, context: RequestContext, event_queue: EventQueue) -> None:
-        pass
+
+        message = {
+            "role": "user" if context.message.role == "user" else "assistant",
+            "content": context.message.parts[0].root.text,
+        }
+        messages = [message]
+
+        response = await self.agent.invoke(messages)
+        await event_queue.enqueue_event(new_agent_text_message(response))
 
     async def cancel(self, context: RequestContext, event_queue: EventQueue) -> None:
-        pass
+        raise Exception("cancel not supported")
 
 
 if __name__ == "__main__":
